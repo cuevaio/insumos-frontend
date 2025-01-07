33085db1679c060c0bff29a616bb725b29eaefc6
jtaco <jtaco@powercosts.com>
2025-01-07 11:43:12 -0500

    feat: export availabilities and insumos in 23, 24, 25 hour template

--- src/components/ExportAvailabilitiesButton/index.tsx
+++ src/components/ExportAvailabilitiesButton/index.tsx
@@ -1,117 +1,104 @@
 import { useMutation } from '@tanstack/react-query';
 import ExcelJS from 'exceljs';
 import FileSaver from 'file-saver';
-import { useTranslation } from 'react-i18next';
 
 import { Button } from '@/components/ui/button';
 
 import { useAvailabilities } from '@/hooks/useAvailabilities';
 import { useFuelTypes } from '@/hooks/useFuelTypes';
-// import { useStore } from '@/hooks/useStore';
 import { useUnits } from '@/hooks/useUnits';
+import { useInsumos } from '@/hooks/useInsumos';
 
 import { populateAvailabilityData, setupWorksheet } from '@/lib/export';
 import { Market } from '@/lib/types';
 
 const ExportAvailabilitiesButton = () => {
-  const {
-    // t,
-    i18n: { language },
-  } = useTranslation();
+	// TODO: Get language using useTranslation hook
+	const language = 'es'
 
-  // TODO: get from localStorage
-  //   const {
-  //     availabilityUnitId: unitId,
-  //     availabilityDate: date,
-  //     availabilityMarket: market,
-  //   } = useStore();
+	const unitId = localStorage.getItem('unit_id');
+	const date = localStorage.getItem('date');
+	const market = localStorage.getItem('market') as Market;
 
-  const unitId = localStorage.getItem('unit_id');
-  const date = localStorage.getItem('date');
-  const market = localStorage.getItem('market') as Market;
+	const { data: units } = useUnits();
+	const { data: fuelTypes } = useFuelTypes();
+	const { data: insumos } = useInsumos({
+		date: date?.toString(),
+		unitId: unitId?.toString(),
+		market,
+	});
 
-  console.log('unitId: ', unitId);
-  console.log('date: ', date);
-  console.log('market: ', market);
+	const { data } = useAvailabilities({ unitId, date, market });
+	const availabilities = data?.availabilities;
 
-  const { data: units } = useUnits();
-  const { data: fuelTypes } = useFuelTypes();
+	const unit = units?.find((u) => u.id === unitId);
 
-  console.log('units: ', units);
-  console.log('fuelTypes: ', fuelTypes);
+	const fuelType1 =
+		unit?.fuelType1ID && fuelTypes?.find((f) => f.id === unit.fuelType1ID);
+	const fuelType2 =
+		unit?.fuelType2ID && fuelTypes?.find((f) => f.id === unit.fuelType2ID);
 
-  const { data } = useAvailabilities({ unitId, date, market });
-  console.log('data: ', data);
-  const availabilities = data?.availabilities;
+	const { mutate } = useMutation({
+		mutationFn: async () => {
+			if (!date) throw new Error('Select a date first!');
+			if (!unit) throw new Error('Select a unit first!');
+			if (!fuelType1) throw new Error('Fuel type 1 not found');
+			if (!availabilities) throw new Error('No availabilities found');
+			if (!insumos) throw new Error('No insumos found');
 
-  const unit = units?.find((u) => u.id === unitId);
+			const duration = data?.dayDurations[date]!;
 
-  // console.log('unit: ', unit)
+			const response = await fetch(`/cpp-ui/template-${duration}.xlsx`);
+			const arrayBuffer = await response.arrayBuffer();
+			const workbook = new ExcelJS.Workbook();
 
-  const fuelType1 =
-    unit?.fuelType1ID && fuelTypes?.find((f) => f.id === unit.fuelType1ID);
-  const fuelType2 =
-    unit?.fuelType2ID && fuelTypes?.find((f) => f.id === unit.fuelType2ID);
+			await workbook.xlsx.load(arrayBuffer);
 
-  const { mutate } = useMutation({
-    mutationFn: async () => {
-      if (!date) throw new Error('Select a date first!');
-      if (!unit) throw new Error('Select a unit first!');
-      if (!fuelType1) throw new Error('Fuel type 1 not found');
-      if (!availabilities) throw new Error('No availabilities found');
+			const templateWorksheet = workbook.getWorksheet(1);
+			if (!templateWorksheet) throw new Error('No template found');
 
-      const duration = data?.dayDurations[date]!;
+			const worksheet = setupWorksheet(
+				templateWorksheet,
+				date,
+				unit,
+				fuelType1,
+				fuelType2 || undefined,
+			);
 
-      const response = await fetch(`/cpp-ui/template-${duration}.xlsx`);
-      const arrayBuffer = await response.arrayBuffer();
-      const workbook = new ExcelJS.Workbook();
-      await workbook.xlsx.load(arrayBuffer);
+			availabilities.forEach((availability, index) => {
+				const insumo = insumos.insumos[index]
 
-      const templateWorksheet = workbook.getWorksheet(1);
-      if (!templateWorksheet) throw new Error('No template found');
+				populateAvailabilityData(
+					worksheet,
+					availability,
+					index,
+					insumo || undefined,
+					fuelType2 || undefined,
+				);
+			});
 
-      const worksheet = setupWorksheet(
-        workbook,
-        templateWorksheet,
-        date,
-        unit,
-        fuelType1,
-        fuelType2 || undefined,
-      );
+			const buffer = await workbook.xlsx.writeBuffer();
+			const blob = new Blob([buffer], {
+				type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+			});
 
-      availabilities.forEach((availability, index) => {
-        populateAvailabilityData(
-          worksheet,
-          availability,
-          index,
-          fuelType2 || undefined,
-        );
-      });
+			FileSaver.saveAs(
+				blob,
+				`RBII-Disponibilidad-${date}-${unit.name.split(' ').join('-')}-${market}-${language.startsWith('es') ? 'ES' : 'EN'}.xlsx`
+			);
+		},
+	});
 
-      workbook.removeWorksheet(1);
-
-      const buffer = await workbook.xlsx.writeBuffer();
-      const blob = new Blob([buffer], {
-        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-      });
-
-      FileSaver.saveAs(
-        blob,
-        `RBII-Disponibilidad-${date}-${unit.name.split(' ').join('-')}-${market}-${language.startsWith('es') ? 'ES' : 'EN'}.xlsx`,
-      );
-    },
-  });
-
-  return (
-    <Button
-      variant="outline"
-      onClick={() => {
-        mutate();
-      }}
-    >
-      Export
-    </Button>
-  );
+	return (
+		<Button
+			variant="outline"
+			onClick={() => {
+				mutate();
+			}}
+		>
+			Export
+		</Button>
+	);
 };
 
 export default ExportAvailabilitiesButton;
