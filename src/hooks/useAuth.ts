9ebbd41be152ae23e08769184618f2c49036e2be
jaimytacovega <jaimy.taco.vega@gmail.com>
2025-03-11 12:05:58 -0500

    feat: auth token by server scope

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,26 +1,31 @@
 import React from 'react';
 
-/* function getCookie(name: string) {
+const getCookie = (name: string) => {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop()!.split(';').shift();
-} */
+  if (parts.length === 2) return parts.pop()!.split(';').shift() ?? '';
+  return ''
+}
 
 export const useAuth = () => {
-  const [token] = React.useState<string>('hi!');
+  const [token, setToken] = React.useState<string>('');
 
-  /* React.useEffect(() => {
-    if (!token) {
+  React.useEffect(() => {
+    if (__SERVER_SCOPE__ === 'gsms'){
+      setToken('no-auth-needed');
+    } else if (!token){
+      // TODO: Define logic for KUBE access token
       const tokenCookie = getCookie('pci_auth_access_token');
       if (tokenCookie) {
         setToken(tokenCookie);
       } else {
-        window.location.replace(
-          'http://localhost:1080/authenticate?username=cpp_pro',
-        );
+        // // TODO: Redirect to login page
+        // window.location.replace(
+        //   'http://localhost:1080/authenticate?username=cpp_pro',
+        // );
       }
     }
-  }, [token]); */
+  }, [token]);
 
   return token;
 };
